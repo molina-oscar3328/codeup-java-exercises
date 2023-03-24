aad60570ad2345f855d4a4c1d78636f07a36918c
Oscar Molina <oscar.molina3328@gmail.com>
2023-03-24 11:35:39 -0500

    java class added

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
+}
+
+int myFavoriteNumber = 28;
+
+System.out.println(myFavoriteNumber);
+
