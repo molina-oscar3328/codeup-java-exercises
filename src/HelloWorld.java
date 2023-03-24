597f21fab4190b2151c9ccb7ae2abdaa66c0ff64
Oscar Molina <oscar.molina3328@gmail.com>
2023-03-24 12:22:56 -0500

    difference between ++i and i++

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,10 +2,30 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
+
+        int myFavoriteNumber = 28;
+
+        System.out.println("myFavoriteNumber");
+
+
+        String myString = "Oscar Molina";
+
+        System.out.println("myString");
+
+
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+        int y = 5;
+        System.out.println(++y);
+        System.out.println(y);
+
+
+
     }
 }
 
-int myFavoriteNumber = 28;
 
-System.out.println(myFavoriteNumber);
+
 
