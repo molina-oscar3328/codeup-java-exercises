250087432a87923a5a7f37145b964dd4ece9f2a6
Oscar Molina <oscar.molina3328@gmail.com>
2023-03-27 09:25:40 -0500

    console IO added

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args) {
+
+//        System.out.println("Hello World");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter something: ");
+        String userInput = scanner.next();
+
+        System.out.println("You entered: --> \"" + userInput + "\" <--");
+
+
+    }
+
+}
+
+
+
+
