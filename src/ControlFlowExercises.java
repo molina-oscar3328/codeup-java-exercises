a85170730a1790545f2200c87393e2c62628b9c6
Oscar Molina <oscar.molina3328@gmail.com>
2023-03-27 14:44:08 -0500

    on fizz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,16 +3,53 @@ public class ControlFlowExercises {
 
         System.out.println("Hello there we will start the exercises:");
 
+//while
 
-        int i = 5;
-        while(i <= 15){
-            System.out.println(i);
-            i++;
-        }
+//        int i = 5;
+//        while(i <= 15){
+//            System.out.print(i + " ");
+//            i++;
+//        }
+
+//do while loop
+
+//        int i = 0;
+//        do{
+//            System.out.println(i);
+//            i += 2;
+//        } while(i <= 100);
 
 
 
+//        int i = 100;
+//        do{
+//            System.out.println(i);
+//            i -= 5;
+//        } while(i >= -10);
 
+//squared numbers below
+
+//        long i = 2;
+//        do{
+//            System.out.println(i);
+//            i *= i;
+//        } while(i <= 1_000_000);
+
+//numbers 1 to 100 below
+
+//        int i = 1;
+//        do{
+//            System.out.println(i);
+//            i++;
+//        } while(i < 101);
+
+//        int i = 1;
+//        for(int i = 1; i <= 100; i++);
+//        if(i % 3 == 0) {
+//            System.out.println("Fizz");
+//        } else{
+//            System.out.println(i);
+//        }
 
 
 
