a1d2a22b2e8bf0d6bd82942f02ec7d332be5a356
Oscar Molina <oscar.molina3328@gmail.com>
2023-04-03 07:50:21 -0500

    my string backslash

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -2,8 +2,18 @@ public class StringExercise {
     public static void main(String[] args) {
 
 
+    String myString;
+    myString = "We don't need no education\nWe don't need no though control";
+    System.out.println(myString);
 
+    myString = "Check \"this\" out!, \"s inside of \"s!";
+    System.out.println(myString);
 
+    myString = "In windows, hte main drive is usually c:\\";
+    System.out.println(myString + "\n");
+
+    myString = "I can do backslashes \\, double backslashes \\\\, \nand the amazing triple backslashes \\\\\\";
+    System.out.println(myString);
 
 
 
