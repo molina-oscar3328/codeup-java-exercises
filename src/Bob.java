f204f2a7a97217be60fc09e8880d8a254f80b4d9
Oscar Molina <oscar.molina3328@gmail.com>
2023-04-03 08:02:04 -0500

    getting Bob ready

---
+++ src/Bob.java
@@ -0,0 +1,25 @@
+public class Bob {
+    public static void main(String[] args) {
+
+        System.out.println("Bob is a lackadaisical teenager.\nHe doesn't really have a lot of friends. Let's talk to him to make friends");
+        System.out.println("Let's start a conversation:"));
+
+        System.out.println("What would you like to talk to Bob about?");
+        sentence = scanner.nextLine():
+
+        if(sentence.endsWith(?)){
+            System.out.println("Bob looks at you with a smile on his face and says: \"Sure.\"");
+        } else if (sentence.endsWith("!")){
+            System.out.println("Bob looks at you in with big eyes and yells: \nWhoa,chill out!");
+        } else if (sentence.equals("")){
+            System.out.println("Bob sighs and says: \"Fine. Be that way!\"");
+        }
+
+
+
+
+
+
+
+    }
+}
